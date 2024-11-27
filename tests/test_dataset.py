fb84c600b6bf05244baa6abe65d207f7b71d133c
Niels de Hoog <njdehoog@gmail.com>
2024-11-27 08:40:14 +0000

    Fix dataset test

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,18 +1,7 @@
-import unittest
 from src.guclimate.core import dataset
 
-class TestCore(unittest.TestCase):
-    def test_open_file(self):
-        ds = dataset.open_dataset(
-            "tests/data/precipitation/anomalies-monthly-means-2022.nc"
-        )
-        self.assertIsInstance(ds, dataset.Dataset)
-
-
-class TestPrecipitationAnomalies(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(5, 5)
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_open_file():
+    ds = dataset.open_dataset(
+        "tests/data/precipitation/anomalies-monthly-means-2022.nc"
+    )
+    assert type(ds) is dataset.Dataset
