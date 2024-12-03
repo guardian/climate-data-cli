8c5d10d7decf747a4f8a72976ba8f7090703c6f4
Niels de Hoog <njdehoog@gmail.com>
2024-12-03 08:37:43 +0000

    Parse time ranges

--- src/guclimate/retrieve/parse_input.py
+++ src/guclimate/retrieve/parse_input.py
@@ -1,17 +1,74 @@
 from guclimate.retrieve.requests import CDSRequest
+from datetime import time
 import re
 
-numericKeys = ["year", "years", "month", "months"]
+numericKeys = ["year", "years", "month", "months", "day", "days"]
 
 def createCDSRequest(config: dict) -> CDSRequest:
     if "product" not in config:
         raise ValueError("Missing product for request")
 
     product = config["product"]
+    params = {}
+
+    # Parse time param
+    time = config.get("time", None)
+    if time is not None:
+        params["time"] = parseTime(time)
+    print(params)
+
+    # Parse numeric params, i.e. day, month, year
     numericParams = {key: parseNumeric(config[key]) for key in numericKeys if key in config}
+    params |= numericParams
+    print(params)
+
     otherParams = {key: config[key] for key in config if key not in numericKeys and key not in ["product", "output"]}
     params = numericParams | otherParams
     return CDSRequest(product, params)
+
+def parseTime(input: str):
+    print(f"parse time {input}")
+    ## Match single time
+    pattern = re.compile("^[0-9]{2}:[0-9]{2}$")
+    if pattern.match(input) is not None:
+        return [input]
+
+    ## Match comma separated times
+    pattern = re.compile(r"^[0-9]{2}:[0-9]{2},\s?[0-9]{2}:[0-9]{2}$")
+    if pattern.match(input) is not None:
+        components = input.split(",")
+        return [time.strip() for time in components]
+
+    ## Match time range
+    range = parseTimeRange(input)
+    if range is not None:
+        return range
+
+    raise ValueError(f"Unable to parse time input: {input}")
+
+
+def parseTimeRange(input: str):
+    pattern = re.compile(r"^[0-9]{2}:[0-9]{2}\s?-\s?[0-9]{2}:[0-9]{2}$")
+    if pattern.match(input) is None:
+        return None
+
+    [start, end] = input.split("-")
+    start_time = time.fromisoformat(start)
+    end_time = time.fromisoformat(end)
+
+    if start_time > end_time:
+        raise ValueError("Start time must be before end time")
+
+    range = []
+    current_time = start_time
+    while current_time <= end_time:
+        range.append(current_time.isoformat(timespec="minutes"))
+        if current_time.hour < 23:
+            current_time = current_time.replace(hour=current_time.hour + 1)
+        else:
+            break
+
+    return range
 
 def parseNumeric(input: str):
     stripped = input.strip()
@@ -38,7 +95,7 @@ def parseInteger(input: str):
 
 
 def parseRange(input: str):
-    pattern = re.compile("^[0-9]{1,4}-[0-9]{1,4}$")
+    pattern = re.compile(r"^[0-9]{1,4}-[0-9]{1,4}$")
     if pattern.match(input) is None:
         return None
 
--- tests/test_parse_input.py
+++ tests/test_parse_input.py
@@ -1,5 +1,5 @@
 import pytest
-from src.guclimate.retrieve.parse_input import parseNumeric, createCDSRequest
+from src.guclimate.retrieve.parse_input import parseNumeric, parseTime, createCDSRequest
 
 numeric_inputs = [
     ("1", "01"),
@@ -8,10 +8,35 @@ numeric_inputs = [
     ("1-3", ["01", "02", "03"]),
 ]
 
-
 @pytest.mark.parametrize("test_input, expected", numeric_inputs)
 def test_parse_numeric(test_input, expected):
     assert parseNumeric(test_input) == expected
+
+
+time_inputs = [
+    ("00:00", ["00:00"]),
+    ("00:00, 02:00", ["00:00", "02:00"]),
+    ("00:00-02:00", ["00:00", "01:00", "02:00"]),
+    ("00:00-23:00", [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ]),
+]
+
+@pytest.mark.parametrize("test_input, expected", time_inputs)
+def test_parse_time(test_input, expected):
+    assert parseTime(test_input) == expected
+
+
+def test_start_time_must_be_before_end_time():
+    with pytest.raises(ValueError):
+        parseTime("23:00-08:00")
 
 
 def test_create_temperature_anomaly_request():
